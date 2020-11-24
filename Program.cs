e6921b3cbf243015f9f0a058b72d4ae12451c6e1
IchiSumeragi <ichisumeragi@hotmail.fr>
2020-11-24 13:00:40 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GachalogDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string downloadPath = Path.GetDirectoryName (System.Reflection.Assembly.GetEntryAssembly().Location);
+            string filePath = downloadPath +"\\list.txt";
+
+            if (!File.Exists(filePath))
+            {
+                Console.Write("Can't find file list.txt in folder " + downloadPath + ". Please make sure the file is placed in the same folder as this exe.\n");
+
+                return;
+            }
+            else
+            {
+                var lines = File.ReadLines(filePath, Encoding.UTF8);
+
+                foreach (var line in lines)
+                {
+                    Regex regex = new Regex(@"\d+");
+
+                    int deckId = int.Parse(regex.Match(line).Value);
+                    string uri = "https://www.gachalog.com/download_image/" + deckId;
+
+                    WebClient client = new WebClient();
+                    Console.WriteLine("Downloading image for deck " + deckId);
+
+                    client.DownloadFile(uri, downloadPath +  "\\" + deckId.ToString() + ".png");
+                }
+            }
+
+            Console.WriteLine("All downloads complete.");
+        }
+    }
+}
